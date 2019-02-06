465b5c1af21574735a3b6642108162e64fe53419
Shubham Singhal <shubham.011190@gmail.com>
2019-02-06 21:52:27 +0530

    Updated with arithmetic operations

--- variables.py
+++ variables.py
@@ -1,1 +1,16 @@
-print("hello world")
+a=12
+b=3
+'''
+print(a+b)
+print(a-b)
+print(a//b)
+'''
+for i in range(1, a//b):
+    print(i)
+
+string= "Hello World!"
+print(string)
+print(string[3])
+print(string[-1])
+print(string[-2])
+print(string[0:4])
